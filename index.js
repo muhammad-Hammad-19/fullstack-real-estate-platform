1c5ad44048162d4fb5b6e485409731d5ac22e5c4
Muhammad Hammad <mh03212868@gmail.com>
2026-05-13 20:10:59 +0500

    change api name

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
-app.use("/api/post/", postRoute);
+app.use("/api/posts/", postRoute);
 app.use("/api/users", userRoute);
 
 connectDB();
