17b5c223f251fc53e6ee945afbb2a1473ded0442
Muhammad Hammad <mh03212868@gmail.com>
2026-09-18 18:10:23 +0500

    allow vercel domains in cors

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.use(
         !origin ||
         allowedOrigins.includes(origin) ||
         origin.startsWith("http://localhost:") ||
-        origin.startsWith("http://127.0.0.1:")
+        origin.startsWith("http://127.0.0.1:") ||
+        origin.endsWith(".vercel.app")
       ) {
         return callback(null, true);
       }
